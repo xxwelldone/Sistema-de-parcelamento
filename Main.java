9486bafbb108bee1d71aef1a908a7aafda83bad0
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-11-16 13:50:24 -0300

    update

--- Main.java
+++ Main.java
@@ -10,21 +10,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        System.out.println("Geração de contrato: ");
-        System.out.println("Valor total do contrato");
-        double totalAmount = sc.nextDouble();
-        Contract contrato = new Contract(new Date(), totalAmount);
+        try{
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Nº contrato:" + contrato.getNumber());
-        System.out.println("Qnt de parcelas: ");
-        int installments = sc.nextInt();
-        ContractService cs = new ContractService();
-        cs.processContract(contrato, installments, new PayPalService());
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            System.out.println("Geração de contrato: ");
+            System.out.println("Valor total do contrato");
+            double totalAmount = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("Data do contrato no formato dd/MM/yyyy");
+            Date date = sdf.parse(sc.nextLine());
+            Contract contrato = new Contract(date, totalAmount);
+
+            System.out.println("Nº contrato:" + contrato.getNumber());
+            System.out.println("Qnt de parcelas: ");
+            int installments = sc.nextInt();
+            ContractService cs = new ContractService();
+            cs.processContract(contrato, installments, new PayPalService());
 
 
-        System.out.println(contrato.getInstallment());
+            System.out.println(contrato.getInstallment());
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+            ;
+        }
 
 
     }
